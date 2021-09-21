7933c17d7d0d07dd46d2a8a7fa0dd6471c59344c
Daniel Fireman <danielfireman@gmail.com>
2021-09-21 12:15:26 -0300

    versao funcionando com proto

---
+++ go.mod
@@ -0,0 +1,12 @@
+module validador
+
+go 1.14
+
+require (
+	github.com/dadosjusbr/coletores v0.0.0-20210827150800-92e4a6a7f133
+	github.com/dadosjusbr/proto v0.0.0-20210921134625-bc3fa5eb0fd7
+	github.com/frictionlessdata/datapackage-go v0.0.0-20210810130302-98883c9586e9
+	github.com/frictionlessdata/tableschema-go v1.1.3
+	google.golang.org/protobuf v1.27.1
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/dadosjusbr/coletores/status"
+	"github.com/dadosjusbr/proto/csv"
+	"github.com/dadosjusbr/proto/pipeline"
+	"github.com/frictionlessdata/datapackage-go/datapackage"
+
+	"google.golang.org/protobuf/proto"
+
+	frictionless "github.com/frictionlessdata/tableschema-go/csv"
+)
+
+var resources = []string{"coleta", "contra_cheque", "remuneracao"}
+
+func main() {
+	// Reading input.
+	in, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		err := status.NewError(status.InvalidInput, fmt.Errorf("error reading execution result from stdin: %q", err))
+		status.ExitFromError(err)
+	}
+	var er pipeline.ResultadoExecucao
+	if err := proto.Unmarshal(in, &er); err != nil {
+		err := status.NewError(status.InvalidInput, fmt.Errorf("error unmarshalling execution result: %q", err))
+		status.ExitFromError(err)
+	}
+
+	// Loading and validating package.
+	pkg, err := datapackage.Load(er.Pr.Pacote)
+	if err != nil {
+		err = status.NewError(status.DataUnavailable, fmt.Errorf("error loading datapackage (%s):%q", er.Pr.Pacote, err))
+		status.ExitFromError(err)
+	}
+
+	for _, v := range resources {
+		sch, err := pkg.GetResource(v).GetSchema()
+		if err != nil {
+			err = status.NewError(status.DataUnavailable, fmt.Errorf("error getting schema from data package resource (%s | %s):%q", er.Pr.Pacote, v, err))
+			status.ExitFromError(err)
+		}
+		if err := sch.Validate(); err != nil {
+			err = status.NewError(status.InvalidInput, fmt.Errorf("error validating schema (%s):%q", er.Pr.Pacote, err))
+			status.ExitFromError(err)
+		}
+
+		switch v {
+		case "coleta":
+			if err := pkg.GetResource(v).Cast(&[]csv.Coleta_CSV{}, frictionless.LoadHeaders()); err != nil {
+				err = status.NewError(status.InvalidInput, fmt.Errorf("error validating datapackage (%s):%q", er.Pr.Pacote, err))
+				status.ExitFromError(err)
+			}
+		case "remuneracao":
+			if err := pkg.GetResource(v).Cast(&[]csv.Remuneracao_CSV{}, frictionless.LoadHeaders()); err != nil {
+				err = status.NewError(status.InvalidInput, fmt.Errorf("error validating datapackage (%s):%q", er.Pr.Pacote, err))
+				status.ExitFromError(err)
+			}
+		default:
+			if err := pkg.GetResource(v).Cast(&[]csv.ContraCheque_CSV{}, frictionless.LoadHeaders()); err != nil {
+				err = status.NewError(status.InvalidInput, fmt.Errorf("error validating datapackage (%s):%q", er.Pr.Pacote, err))
+				status.ExitFromError(err)
+			}
+		}
+
+	}
+
+	// Printing output.
+	out, err := json.MarshalIndent(er, "", "  ")
+	if err != nil {
+		err = status.NewError(status.OutputError, fmt.Errorf("error marshaling output:%q", err))
+		status.ExitFromError(err)
+	}
+	fmt.Print(string(out))
+}
