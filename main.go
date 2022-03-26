84d32a254eead87d21015229c33d21f94697171f
Daniel Fireman <danielfireman@gmail.com>
2022-03-26 10:01:29 -0300

    Usando dadosjusbr/datapackage

--- main.go
+++ main.go
@@ -6,15 +6,11 @@ import (
 	"os"
 
 	"github.com/dadosjusbr/coletores/status"
+	"github.com/dadosjusbr/datapackage"
 	"github.com/dadosjusbr/proto/pipeline"
-	"github.com/frictionlessdata/datapackage-go/datapackage"
 
 	"google.golang.org/protobuf/encoding/prototext"
-
-	frictionless "github.com/frictionlessdata/tableschema-go/csv"
 )
-
-var resources = []string{"coleta", "contra_cheque", "remuneracao"}
 
 func main() {
 	// Reading input.
@@ -29,52 +25,10 @@ func main() {
 		status.ExitFromError(err)
 	}
 
-	// Loading and validating package.
-	pkg, err := datapackage.Load(er.Pr.Pacote)
-	if err != nil {
+	// Loading and validating package.xs
+	if _, err := datapackage.Load(er.Pr.Pacote); err != nil {
 		err = status.NewError(status.DataUnavailable, fmt.Errorf("error loading datapackage (%s):%q", er.Pr.Pacote, err))
 		status.ExitFromError(err)
-	}
-
-	for _, v := range resources {
-		sch, err := pkg.GetResource(v).GetSchema()
-		if err != nil {
-			err = status.NewError(status.DataUnavailable, fmt.Errorf("error getting schema from data package resource (%s | %s):%q", er.Pr.Pacote, v, err))
-			status.ExitFromError(err)
-		}
-		if err := sch.Validate(); err != nil {
-			err = status.NewError(status.InvalidInput, fmt.Errorf("error validating schema (%s):%q", er.Pr.Pacote, err))
-			status.ExitFromError(err)
-		}
-
-		switch v {
-		case "coleta":
-			if err := pkg.GetResource(v).Cast(&[]Coleta_CSV{}, frictionless.LoadHeaders()); err != nil {
-				err = status.NewError(status.InvalidInput, fmt.Errorf("error validating datapackage (%s):%q", er.Pr.Pacote, err))
-				status.ExitFromError(err)
-			}
-		case "remuneracao":
-			if err := pkg.GetResource(v).Cast(&[]Remuneracao_CSV{}, frictionless.LoadHeaders()); err != nil {
-				err = status.NewError(status.InvalidInput, fmt.Errorf("error validating datapackage (%s):%q", er.Pr.Pacote, err))
-				status.ExitFromError(err)
-			}
-		case "metadados":
-			if err := pkg.GetResource(v).Cast(&[]Metadados_CSV{}, frictionless.LoadHeaders()); err != nil {
-				err = status.NewError(status.InvalidInput, fmt.Errorf("error validating datapackage (%s):%q", er.Pr.Pacote, err))
-				status.ExitFromError(err)
-			}
-		case "contra_cheque":
-			if err := pkg.GetResource(v).Cast(&[]ContraCheque_CSV{}, frictionless.LoadHeaders()); err != nil {
-				err = status.NewError(status.InvalidInput, fmt.Errorf("error validating datapackage (%s):%q", er.Pr.Pacote, err))
-				status.ExitFromError(err)
-			}
-		default:
-			if err := pkg.GetResource(v).Cast(&[]ContraCheque_CSV{}, frictionless.LoadHeaders()); err != nil {
-				err = status.NewError(status.InvalidInput, fmt.Errorf("error validating datapackage (%s):%q", er.Pr.Pacote, err))
-				status.ExitFromError(err)
-			}
-		}
-
 	}
 
 	// Printing output.
